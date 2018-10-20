23c3182798f11ebb496af15cd9a2db739dd24153
Atem <cr0nic1312@gmail.com>
2018-10-20 19:48:23 +0200

    added doggo command

--- cogs/rarted.py
+++ cogs/rarted.py
@@ -3,17 +3,22 @@ from discord.ext import commands
 import random
 import aiohttp
 import asyncio
+import json
 
-
-#fetch for the insult command
-async def fetch(session, url):
+#fetch with plain text
+async def fetch_text(session, url):
     async with session.get(url) as response:
         return await response.text()
+
+#fetch with json data
+async def fetch_json(session, url):
+    async with session.get(url) as response:
+        return await response.json()
 
 
 #insult api
 insult_url = "https://insult.mattbas.org/api/insult"
-dog_url = "https://dog.ceo/api/breeds/image/random "
+dog_url = "http://shibe.online/api/shibes?"
 
 
 class RartedCog:
@@ -24,16 +29,20 @@ class RartedCog:
     @commands.command(name="insult")
     async def insult(self, ctx, *, user_insulted: str):
         async with aiohttp.ClientSession() as session:
-            raw_text = await fetch(session, insult_url)
+            raw_text = await fetch_text(session, insult_url)
             text_1 = raw_text.replace("You", user_insulted, 1)
             final_phrase = text_1.replace("are", "is", 1)
             await ctx.send(final_phrase)
 
-    @commands.command(name="Doggo")
-    async def doggo(self,ctx)
-    async with aiohttp.ClientSession() as session:
-        dog_picture = await fetch(session, dog_url)
-        await ctx.send(dog_picture)
+    @commands.command(name="doggo")
+    async def doggo(self, ctx):
+        async with aiohttp.ClientSession() as session:
+            dog_picture = await fetch_json(session, dog_url)
+
+            embed = discord.Embed(colour=0x98FB98)
+            embed.set_image(url=dog_picture[0])
+
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
