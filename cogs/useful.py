53afde10940a6db9cf24eb134c8e49b8accc49a5
Atem <cr0nic1312@gmail.com>
2019-01-13 20:20:21 +0100

    bmi

--- cogs/useful.py
+++ cogs/useful.py
@@ -1,6 +1,24 @@
 import discord
 from discord.ext import commands
+import math
 
+
+# async def bmi_man(bmi):
+#     if bmi < 20:
+#         await ctx.send("you're underweight")
+#     elif 19 < bmi < 26:
+#         await ctx.send("normal weight")
+#     elif bmi > 25:
+#         await ctx.send("you're are overweight")
+
+
+# async def bmi_woman(bmi):
+#     if bmi < 19:
+#         await ctx.send("you're underweight")
+#     elif 18 < bmi < 25:
+#         await ctx.send("normal weight")
+#     elif bmi > 24:
+#         await ctx.send("you're are overweight")
 
 class UsefulCog:
     def __init__(self, bot):
@@ -15,6 +33,14 @@ class UsefulCog:
             await ctx.send("deleted {} messages" .format(amount))
 
 
+    # @commands.command(name="bmi")
+    # async def bodymassindex (self, ctx, weight: int, height: float, gender):
+    #     bmi = weight // math.pow(height, 2) 
+    #     await ctx.send(bmi)
+    #     if gender == "m":
+    #         bmi_man(bmi)
+    #     elif gender == "f":
+    #         bmi_woman(bmi)
 
 
     # make a help command
